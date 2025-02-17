1cca260ddc56aa9d219de713ac08bb52844841c8
sbug51 <arzo.arsalan@gmail.com>
2025-02-17 13:21:10 +0400

    moving from linux to windows

--- readline/term_linux.go
+++ readline/term_linux.go
@@ -1,31 +1,35 @@
-//go:build linux || solaris || windows
-// +build linux solaris windows
+//go:build linux || windows
+// +build linux windows
 
-package readline
+package discover
 
 import (
-	"syscall"
-	"unsafe"
+	"log/slog"
+	"runtime"
+	"strings"
 )
 
-const (
-	tcgets = 0x5401
-	tcsets = 0x5402
-)
-
-func getTermios(fd uintptr) (*Termios, error) {
-	termios := new(Termios)
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd, tcgets, uintptr(unsafe.Pointer(termios)), 0, 0, 0)
-	if err != 0 {
-		return nil, err
+// oneapiGetVisibleDevicesEnv returns the environment variable and its value
+// for selecting oneAPI (Level Zero) devices. On Windows, if needed, you can adjust
+// the prefix; for now, we use the same "level_zero:" prefix as on Linux.
+func oneapiGetVisibleDevicesEnv(gpuInfo []GpuInfo) (string, string) {
+	ids := []string{}
+	for _, info := range gpuInfo {
+		if info.Library != "oneapi" {
+			// This shouldn't happen if things are wired correctly.
+			slog.Debug("oneapiGetVisibleDevicesEnv skipping over non-sycl device", "library", info.Library)
+			continue
+		}
+		ids = append(ids, info.ID)
 	}
-	return termios, nil
-}
 
-func setTermios(fd uintptr, termios *Termios) error {
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd, tcsets, uintptr(unsafe.Pointer(termios)), 0, 0, 0)
-	if err != 0 {
-		return err
+	// Set the prefix for the oneAPI device selector.
+	// On Windows, adjust this if necessary.
+	prefix := "level_zero:"
+	if runtime.GOOS == "windows" {
+		// If Windows requires a different prefix, change it here.
+		prefix = "level_zero:" // For now, we assume it's the same.
 	}
-	return nil
+
+	return "ONEAPI_DEVICE_SELECTOR", prefix + strings.Join(ids, ",")
 }
