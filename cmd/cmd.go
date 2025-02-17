6f96ee30886665999e544aabe17ace621cc0b0de
sbug51 <arzo.arsalan@gmail.com>
2025-02-17 20:10:36 +0400

    Moving from linux to windows, and fixing the missing engine

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -341,7 +341,7 @@ func RunHandler(cmd *cobra.Command, args []string) error {
 	// TODO(jessegross): We should either find another way to know if this is
 	// a vision model or remove the logic. Also consider that other modalities will
 	// need different behavior anyways.
-	opts.MultiModal = len(info.ProjectorInfo) != 0 || envconfig.NewEngine()
+	engine := envconfig.AsMap() // If your code expects a map
 	opts.ParentModel = info.Details.ParentModel
 
 	if interactive {
