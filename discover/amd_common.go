bb0a15c966a8103ba99cc2e856e27ff75b8358e5
sbug51 <arzo.arsalan@gmail.com>
2025-02-17 09:21:35 +0400

    fixing of linux and windows package

--- discover/amd_common.go
+++ discover/amd_common.go
@@ -1,4 +1,5 @@
 //go:build linux || windows
+// +build linux windows
 
 package discover
 
@@ -11,7 +12,7 @@ import (
 	"strings"
 )
 
-// Determine if the given ROCm lib directory is usable by checking for existence of some glob patterns
+// rocmLibUsable determines if the given ROCm lib directory is usable by checking for the existence of some glob patterns.
 func rocmLibUsable(libDir string) bool {
 	slog.Debug("evaluating potential rocm lib dir " + libDir)
 	for _, g := range ROCmLibGlobs {
@@ -23,6 +24,7 @@ func rocmLibUsable(libDir string) bool {
 	return true
 }
 
+// GetSupportedGFX returns a list of supported GFX types from the ROCm library directory.
 func GetSupportedGFX(libDir string) ([]string, error) {
 	var ret []string
 	files, err := filepath.Glob(filepath.Join(libDir, "rocblas", "library", "TensileLibrary_lazy_gfx*.dat"))
@@ -35,6 +37,7 @@ func GetSupportedGFX(libDir string) ([]string, error) {
 	return ret, nil
 }
 
+// commonAMDValidateLibDir attempts to locate a valid ROCm library directory.
 func commonAMDValidateLibDir() (string, error) {
 	// Favor our bundled version
 
@@ -46,38 +49,4 @@ func commonAMDValidateLibDir() (string, error) {
 	}
 
 	// Prefer explicit HIP env var
-	hipPath := os.Getenv("HIP_PATH")
-	if hipPath != "" {
-		hipLibDir := filepath.Join(hipPath, "bin")
-		if rocmLibUsable(hipLibDir) {
-			slog.Debug("detected ROCM via HIP_PATH=" + hipPath)
-			return hipLibDir, nil
-		}
-	}
-
-	// Scan the LD_LIBRARY_PATH or PATH
-	pathEnv := "LD_LIBRARY_PATH"
-	if runtime.GOOS == "windows" {
-		pathEnv = "PATH"
-	}
-
-	paths := os.Getenv(pathEnv)
-	for _, path := range filepath.SplitList(paths) {
-		d, err := filepath.Abs(path)
-		if err != nil {
-			continue
-		}
-		if rocmLibUsable(d) {
-			return d, nil
-		}
-	}
-
-	// Well known location(s)
-	for _, path := range RocmStandardLocations {
-		if rocmLibUsable(path) {
-			return path, nil
-		}
-	}
-
-	return "", errors.New("no suitable rocm found, falling back to CPU")
-}
+	hipPath :
